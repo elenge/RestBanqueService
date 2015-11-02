327018a2489e40dbdb9d73c1c12f25d508f51c95
AlexandreBodin <arpbodin@gmail.com>
2015-11-02 01:55:31 +0100

    Ajout d'une classe de reset du fichier comptes.dat pour réinitialiser la base + test des java

---
+++ src/test/WebServiceTester.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import com.ueve.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+public class WebServiceTester {
+
+		
+		
+
+		   public static void main(String[] args){
+			   
+			  //supprimer le fichier pour reset la base local
+			  File comptesDat = new File("comptes.dat");
+			  if(comptesDat.exists()){
+				  comptesDat.delete();
+			  }
+			  
+			  //supprimer le fichier pour reset la base tomcat
+			  
+			  File comptesDat2 = new File("E:/arpbo/Developement/Apache Software Foundation/Tomcat 7.0/comptes.dat");
+			  System.out.println(comptesDat2.exists());
+			  if(comptesDat2.exists()){
+				  comptesDat2.delete();
+				  System.out.println("Suppression du comptes.dat de l'arborescence Tomcat");
+			  }
+			  
+			   //création d'un compte DAO
+//			  CompteDao cDao = new CompteDao();
+//			  ArrayList<CompteBancaire> compteList = (ArrayList<CompteBancaire>) cDao.getAllComptes();
+//			  System.out.println("Taille de la liste : "+compteList.size()+"\n"+compteList);
+//			  System.out.println("---------------------------------------------------------------------");
+			  
+			 
+			  
+			  //on test l'ajout d'un compte DAO
+//			  cDao.addCompte("Michel");
+//			  ArrayList<CompteBancaire> compteList2 = (ArrayList<CompteBancaire>) cDao.getAllComptes();
+//			  System.out.println("Taille de la liste : "+compteList2.size()+"\n"+compteList2);
+//			  System.out.println("---------------------------------------------------------------------");
+			  
+			  
+		   }
+
+		 
+}
